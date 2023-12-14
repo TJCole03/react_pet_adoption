a05534128b02bce9475f73893bf070c50bf7b9bd
TJCole03 <tjc6254@gmail.com>
2023-12-14 06:42:48 -0500

    Error Boundary lesson finished

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import { useQuery } from '@tanstack/react-query'
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from './fetchPets'
 import Carousel from "./Carousel";
 
@@ -21,6 +22,9 @@ const Details = () => {
     const pet = results.data.pets[0];
 
     return (
+        //tempting to make the error boundary wrap around this entire block of code but 
+        //DO NOT DO THAT; WHY?
+        //error boundaries can only catch things in components that are rendered inside of it
         <div className="details">
             <Carousel images={pet.images} />
             <div>
@@ -32,6 +36,19 @@ const Details = () => {
             </div>
         </div>
     )
-  };
+};
   
-  export default Details;
+function DetailsErrorBoundary(props) {
+    return (
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    )
+}
+//So our details page doesn't actually accept any props up here right ? But what is one problem with this if for whatever reason we startedpassing in props to details? 
+//as of right now they will get killed right here right ? because we are not passing these props into details.So this would be like one of the only cases I'd be okay withyou 
+//just saying ...props here using the spread operator here, because details error boundary doesn't care about these props at all.It's meant to be totally 
+//seamless andpassed these props through seamlessly, right ? So this is what I would do here is basically say like I don't care what
+//props are coming in just go directly through the ErrorBoundary anddon't care about what's coming in, right ?
+  
+  export default DetailsErrorBoundary;
