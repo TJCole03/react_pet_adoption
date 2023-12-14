83bacc180c5a643e4427eb2db2b26c0cd2f291c1
TJCole03 <tjc6254@gmail.com>
2023-12-14 06:05:38 -0500

    updated Pet.jsx and two bugs. one in fetchSearch and one in fetchBreedList

--- src/fetchPets.js
+++ src/fetchPets.js
@@ -1,6 +1,6 @@
 const fetchPet = async ({ queryKey }) => {
     const id = queryKey[1];
-    const apiRes = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`, {mode: 'no-cors'});
+    const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`, {mode: 'no-cors'});
   
     if (!apiRes.ok) {
       throw new Error(`details/${id} not fetching`);
