9330827ec8c52dc8d12bf2a29439ce641eec429f
TJCole03 <tjc6254@gmail.com>
2024-01-09 07:15:33 -0500

    more debug attempts. issued pull request

--- src/fetchBreedList.jsx
+++ src/fetchBreedList.jsx
@@ -1,17 +1,18 @@
 async function fetchBreedList({ queryKey }) {
-    const animal = queryKey[1];
-  
-    if (!animal) return [];
-  
-    const apiRes = await fetch(
-      `https://pets-v2.dev-apis.com/breeds?animal=${animal}`, {mode: 'no-cors'}
-    );
-  
-    if (!apiRes.ok) {
-      throw new Error(`breeds ${animal} not fetching`);
-    }
-  
-    return apiRes.json();
+  const animal = queryKey[1];
+
+  if (!animal) return [];
+
+  const res = await fetch(
+    `https://pets-v2.dev-apis.com/breeds?animal=${animal}`,
+    {mode: 'no-cors'}
+  );
+
+  if (!res.ok) {
+    throw new Error(`breeds ${animal} fetch not ok`);
   }
-  
-  export default fetchBreedList;
+
+  return res.json();
+}
+
+export default fetchBreedList;
